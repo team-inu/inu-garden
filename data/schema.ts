88bc04ecd4cb760cf8cd0b30f0a20843bfeec258
Porping <porsteam@hotmail.com>
2023-11-25 21:58:36 +0700

    feat: add new admission schema

--- data/schema.ts
+++ data/schema.ts
@@ -10,3 +10,20 @@ export const StudentSchema = z.object({
 })
 
 export type Student = z.infer<typeof StudentSchema>
+
+export const AdmissionSchema = z.object({
+  id: z.string(),
+  studentId: z.string(),
+  firstName: z.string(),
+  lastName: z.string(),
+  admission: z.string(),
+  email: z.string(),
+  GPAX: z.number(),
+  mathGPA: z.number(),
+  englishGPA: z.number(),
+  scienceGPA: z.number(),
+  school: z.string(),
+  city: z.string(),
+})
+
+export type Admission = z.infer<typeof AdmissionSchema>
