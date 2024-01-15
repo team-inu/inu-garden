4875d2494bbe49fd391de0f77278ea8f4bf5d860
Porping <porsteam@hotmail.com>
2024-01-15 19:27:46 +0700

    feat: add assignment table

---
+++ components/features/course/assignment/assignment-table.tsx
@@ -0,0 +1,135 @@
+"use client"
+
+import * as React from "react"
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFacetedRowModel,
+  getFacetedUniqueValues,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table"
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+import { DataTablePagination } from "@/components/ui/data-table-pagination"
+import { DataTableToolbar} from "@/components/ui/data-table-toolbar"
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[]
+  data: TData[]
+  getValues?: (id: string) => void
+}
+
+
+
+export function AssignmentDataTable<TData, TValue>({
+  columns,
+  data,
+  getValues,
+}: DataTableProps<TData, TValue>) {
+  const [rowSelection, setRowSelection] = React.useState({})
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({})
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  )
+  const [sorting, setSorting] = React.useState<SortingState>([])
+
+  const table = useReactTable({
+    data,
+    columns,
+    state: {
+      sorting,
+      columnVisibility,
+      rowSelection,
+      columnFilters,
+    },
+    enableRowSelection: true,
+    onRowSelectionChange: setRowSelection,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    onColumnVisibilityChange: setColumnVisibility,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFacetedRowModel: getFacetedRowModel(),
+    getFacetedUniqueValues: getFacetedUniqueValues(),
+  })
+
+  return (
+    <div className="space-y-4">
+      <DataTableToolbar table={table} selectorOptions={[]}  />
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id} colSpan={header.colSpan}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  )
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody className="cursor-pointer">
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                  onClick={() => {
+                    if (getValues) {
+                      getValues(row.getValue("name"))
+                    }
+                  }}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <DataTablePagination table={table} isRowSelectionEnabled={false}/>
+    </div>
+  )
+}
---
+++ components/features/course/assignment/assignment.tsx
@@ -0,0 +1,124 @@
+"use client";
+
+import { useState } from "react";
+import { columns as assignmentColumns } from "./assignment-column";
+import { columns as scoreColumns } from "../score/score-column";
+import { AssignmentDataTable } from "./assignment-table";
+import { ScoreDataTable } from "../score/score-table";
+import Image from "next/image";
+import PieChartCustom from "@/components/piechart";
+import RadialBarChartCustom from "@/components/radial-bar-chart";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import ScatterChartCustom from "@/components/scatter-chart";
+import { Button } from "@/components/ui/button";
+
+const Assignment = () => {
+  const [selectedRows, setSelectedRows] = useState<string>("");
+  const getVales = (id: string) => {
+    setSelectedRows(id);
+  };
+  return (
+    <div className="space-y-5">
+      <h1 className="text-2xl font-bold mb-5">Assignments</h1>
+      <div className="">
+        <AssignmentDataTable
+          columns={assignmentColumns}
+          getValues={getVales}
+          data={[
+            {
+              id: "1",
+              name: "Assignment 1",
+              clo: "CLO1, CLO2",
+              plo: "PLO1",
+              po: "PO1",
+              dueDate: "20/10/2021",
+              percentage: "20%",
+              weigth: "20%",
+            },
+            {
+              id: "2",
+              name: "Assignment 2",
+              clo: "CLO2",
+              plo: "PLO2",
+              po: "PO2",
+              dueDate: "20/10/2021",
+              percentage: "20%",
+              weigth: "20%",
+            },
+            {
+              id: "3",
+              name: "Assignment 3",
+              clo: "CLO3",
+              plo: "PLO3",
+              po: "PO3",
+              dueDate: "20/10/2021",
+              percentage: "20%",
+              weigth: "20%",
+            },
+          ]}
+        />
+      </div>
+      {selectedRows ? (
+        <div className="grid gap-3 grid-cols-2">
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle>Score</CardTitle>
+              <CardDescription>
+                <Button className="" variant={"secondary"}>Import Score</Button>
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <ScoreDataTable
+                columns={scoreColumns}
+                data={[
+                  {
+                    id: "1",
+                    studentId: "600612345",
+                    firstName: "John",
+                    lastName: "Doe",
+                    score: 100,
+                  },
+                ]}
+              />
+            </CardContent>
+          </Card>
+          <div>
+            <Card>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle>Summary</CardTitle>
+                <CardDescription>Score of {selectedRows}</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <ScatterChartCustom />
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+      ) : (
+        <div className="flex flex-col space-y-5 mt-10 items-center justify-center">
+          <Image
+            priority
+            src="/images/shiba.svg"
+            alt="shiba"
+            width={160}
+            height={160}
+            className="animate-pulse"
+            placeholder="blur"
+            blurDataURL="/images/shiba.svg"
+          />
+          <h1 className="text-xl font-bold mb-5 text-slate-700">
+            Please select assignment to see score
+          </h1>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Assignment;
