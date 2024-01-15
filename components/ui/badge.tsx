ce290e42bf4c40f3d88f4443aeedc58eaf20a46b
Porping <porsteam@hotmail.com>
2024-01-15 19:34:20 +0700

    refactor: add new variant in badge

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -15,6 +15,8 @@ const badgeVariants = cva(
         destructive:
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
         outline: "text-foreground",
+        green: "border-transparent bg-green-600 text-green-foreground",
+        warning: "border-transparent bg-yellow-600 text-warning-foreground",
       },
     },
     defaultVariants: {
