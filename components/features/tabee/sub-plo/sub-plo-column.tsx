9583ffb6275585de9f4cfec074b2f784741e43fc
Porping <porsteam@hotmail.com>
2024-01-15 19:31:58 +0700

    feat: add sub plo table

---
+++ components/features/tabee/sub-plo/sub-plo-column.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { Checkbox } from "@/components/ui/checkbox";
+import {   SubPLO } from "@/data/schema";
+import { DataTableColumnHeader } from "@/components/ui/data-table-column-header";
+import { SubPLORowActions } from "./sub-plo-row-action";
+
+
+export const columns: ColumnDef<SubPLO>[] = [
+  {
+    accessorKey: "id",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="id" />
+    ),
+    cell: ({ row }) => <div className="">{row.getValue("id")}</div>,
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "descriptionThai",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="descriptionThai" />
+    ),
+    cell: ({ row }) => (
+      <div className="w-[80px]">{row.getValue("descriptionThai")}</div>
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "descriptionEnglish",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="descriptionEnglish" />
+    ),
+    cell: ({ row }) => (
+      <div className="w-[80px]">{row.getValue("descriptionEnglish")}</div>
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  
+  {
+    id: "actions",
+    cell: ({ row }) => <SubPLORowActions row={row} />,
+  },
+];
