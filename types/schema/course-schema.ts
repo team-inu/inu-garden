068d78e9ef17f979d313adbe8b8dcdc217f4f1c5
Porping <porsteam@hotmail.com>
2024-01-17 13:07:53 +0700

    feat: add defaultvalue create course schema

--- types/schema/course-schema.ts
+++ types/schema/course-schema.ts
@@ -2,61 +2,71 @@ import * as z from "zod";
 
 export const CreateCourseSchema = z.object({
   name: z
-    .string({ required_error: "Please enter a name" })
-    .min(1, { message: "Please enter a name" }),
+    .string({ required_error: "required" })
+    .min(1, { message: "required" }),
   description: z
-    .string({ required_error: "Please enter a description" })
-    .min(1, { message: "Please enter a description" }),
+    .string({ required_error: "required" })
+    .min(1, { message: "required" }),
   code: z
-    .string({ required_error: "Please enter a code" })
-    .min(1, { message: "Please enter a code" }),
+    .string({ required_error: "required" })
+    .min(1, { message: "required" }),
   lecturer: z
-    .string({ required_error: "Please enter a lecturer" })
-    .min(1, { message: "Please enter a lecturer" }),
+    .string({ required_error: "required" })
+    .min(1, { message: "required" }),
   semester: z
-    .string({ required_error: "Please enter a semester" })
-    .min(1, { message: "Please enter a semester" }),
+    .string({ required_error: "required" })
+    .min(1, { message: "required" }),
   courseLearningOutcome: z
     .array(
       z.object({
-        code: z.string({ required_error: "Please enter a code" }).min(1, {
-          message: "Please enter a code",
+        code: z.string({ required_error: "required" }).min(1, {
+          message: "required",
         }),
         weight: z
-          .string({ required_error: "Please enter a weight" })
-          .min(1, { message: "Please enter a weight" }),
+          .string({ required_error: "required" })
+          .min(1, { message: "required" }),
         description: z
-          .string({ required_error: "Please enter a description" })
-          .min(1, { message: "Please enter a description" }),
+          .string({ required_error: "required" })
+          .min(1, { message: "required" }),
         subProgramLearningOutcome: z
-          .string({ required_error: "Please enter a program learning outcome" })
-          .min(1, { message: "Please enter a program learning outcome" }),
+          .string({ required_error: "required" })
+          .min(1, { message: "required" }),
         programOutcome: z
-          .string({ required_error: "Please enter a program outcome" })
-          .min(1, { message: "Please enter a program outcome" }),
+          .string({ required_error: "required" })
+          .min(1, { message: "required" }),
       })
     )
     .min(1, { message: "Please enter at least one course learning outcome" }),
   grade: z.object({
     a: z
-      .number({ required_error: "Please enter A grade" })
-      .min(0, { message: "Please enter A grade" }),
+      .string({ required_error: "Please enter A grade" })
+      .min(1, { message: "Please enter A grade" }),
     b: z
-      .number({ required_error: "Please enter B grade" })
-      .min(0, { message: "Please enter B grade" }),
+      .string({ required_error: "Please enter B grade" })
+      .min(1, { message: "Please enter B grade" }),
     c: z
-      .number({ required_error: "Please enter C grade" })
-      .min(0, { message: "Please enter C grade" }),
+      .string({ required_error: "Please enter C grade" })
+      .min(1, { message: "Please enter C grade" }),
     d: z
-      .number({ required_error: "Please enter D grade" })
-      .min(0, { message: "Please enter D grade" }),
+      .string({ required_error: "Please enter D grade" })
+      .min(1, { message: "Please enter D grade" }),
     f: z
-      .number({ required_error: "Please enter F grade" })
-      .min(0, { message: "Please enter F grade" }),
+      .string({ required_error: "Please enter F grade" })
+      .min(1, { message: "Please enter F grade" }),
   }),
 });
 
 export type CreateCourseSchemaValues = z.infer<typeof CreateCourseSchema>;
 
 export const CreateCourseSchemaDefaultValues: Partial<CreateCourseSchemaValues> =
-  {};
+  {
+    courseLearningOutcome: [
+      {
+        code: "",
+        weight: "",
+        description: "",
+        subProgramLearningOutcome: "",
+        programOutcome: "",
+      },
+    ],
+  };
