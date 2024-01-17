77abe4c9c64c1d3f18e13f0f3679c9fd7b33695d
Porping <porsteam@hotmail.com>
2024-01-17 13:06:54 +0700

    refactor: change color error message form

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -93,7 +93,7 @@ const FormLabel = React.forwardRef<
   return (
     <Label
       ref={ref}
-      className={cn(error && "text-destructive", className)}
+      className={cn(error && "text-red-500", className)}
       htmlFor={formItemId}
       {...props}
     />
@@ -156,7 +156,7 @@ const FormMessage = React.forwardRef<
     <p
       ref={ref}
       id={formMessageId}
-      className={cn("text-sm font-medium text-destructive", className)}
+      className={cn("text-sm font-medium text-red-500", className)}
       {...props}
     >
       {body}
