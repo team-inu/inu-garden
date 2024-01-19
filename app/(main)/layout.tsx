49f50bfd9b5c6465373787e7282e5ac6bb84bde6
XiaoXuxxxx <73125291+XiaoXuxxxx@users.noreply.github.com>
2024-01-20 00:22:56 +0700

    refactor: remove tree provider
    Co-authored-by: Porping <porsteam@hotmail.com>

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -19,10 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <Navbar />
-          {children}
-        </ThemeProvider>
+        <Navbar />
+        {children}
       </body>
     </html>
   );
