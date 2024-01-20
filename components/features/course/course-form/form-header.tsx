7259ffa97cdcf4070580a574bb783571c1f2415c
Porping <porsteam@hotmail.com>
2024-01-20 15:09:54 +0700

    refactor: file import functionality and fix formatting issues

--- components/features/course/course-form/form-header.tsx
+++ components/features/course/course-form/form-header.tsx
@@ -1,22 +1,21 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
-import { Input } from '@/components/ui/input';
-import { tableToObject, worksheetToTables } from '@/lib/excel';
-import { CreateCourseSchemaValues } from '@/types/schema/course-schema';
+import { Input } from "@/components/ui/input";
+import { tableToObject, worksheetToTables } from "@/lib/excel";
+import { CreateCourseSchemaValues } from "@/types/schema/course-schema";
 import { TimerIcon, ImportIcon } from "lucide-react";
-import { ChangeEvent, useRef } from 'react';
-import { useFormContext } from 'react-hook-form';
-import { toast } from 'sonner';
-import * as XLSX from 'xlsx';
-
+import { ChangeEvent, useRef } from "react";
+import { useFormContext } from "react-hook-form";
+import { toast } from "sonner";
+import * as XLSX from "xlsx";
 
 const CourseFormHeader = () => {
-  const formCtx = useFormContext<CreateCourseSchemaValues>()
+  const formCtx = useFormContext<CreateCourseSchemaValues>();
   const fileImportRef = useRef<HTMLInputElement>(null);
   const handleUpload = async (e: ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0]
+    const file = e.target.files?.[0];
     if (!file) {
-      return toast.error('Can not read file');
+      return toast.error("Can not read file");
     }
 
     const buffer = await file.arrayBuffer();
@@ -30,26 +29,34 @@ const CourseFormHeader = () => {
     const clo = tableToObject(CLOTable[0], CLOTable.slice(1));
 
     formCtx.reset({
-      name: info['CourseTitle'],
-      code: info['_CourseID'],
-      semester: info['Semester'],
-      lecturer: 'a',
-      description: '',
+      name: info["CourseTitle"],
+      code: info["_CourseID"],
+      semester: info["Semester"],
+      lecturer: "a",
+      description: "",
       courseLearningOutcome: clo.map((c) => ({
-        code: c['_No.'],
-        description: c['Course Learning Outcomes (CLOs)'],
-        programOutcome: c['KMUTT PLO'],
-        subProgramLearningOutcome: '',
-        weight: '',
-      }))
-    })
-  }
+        code: c["_No."],
+        description: c["Course Learning Outcomes (CLOs)"],
+        programOutcome: c["KMUTT PLO"],
+        subProgramLearningOutcome: "",
+        weight: "",
+      })),
+    });
+  };
 
   return (
     <div className="space-x-5">
-      <Input type='file' className="hidden" ref={fileImportRef} onChange={handleUpload} />
-      <Button variant={"secondary"} className="space-x-3"
-        onClick={() => fileImportRef.current?.click()}>
+      <Input
+        type="file"
+        className="hidden"
+        ref={fileImportRef}
+        onChange={handleUpload}
+      />
+      <Button
+        variant={"secondary"}
+        className="space-x-3"
+        onClick={() => fileImportRef.current?.click()}
+      >
         <ImportIcon className="w-5 h-5" />
         <div className="">Import</div>
       </Button>
