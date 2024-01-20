879ce8318370f56f87a29465906373c4377b2d99
Porping <porsteam@hotmail.com>
2024-01-20 15:11:12 +0700

    feat: Add score upload functionality score table

--- components/features/course/score/score-table.tsx
+++ components/features/course/score/score-table.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as XLSX from "xlsx";
 import * as React from "react";
 import {
   ColumnDef,
@@ -27,17 +28,23 @@ import {
 
 import { DataTablePagination } from "@/components/ui/data-table-pagination";
 import { DataTableToolbar } from "@/components/ui/data-table-toolbar";
+import { toast } from "sonner";
+import { tableToObject, worksheetToTables } from "@/lib/excel";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
   data: TData[];
   getValues?: (id: string) => void;
+  assignmentName?: string;
+  assignmentId?: string;
 }
 
 export function ScoreDataTable<TData, TValue>({
   columns,
   data,
   getValues,
+  assignmentName,
+  assignmentId,
 }: DataTableProps<TData, TValue>) {
   const [rowSelection, setRowSelection] = React.useState({});
   const [columnVisibility, setColumnVisibility] =
@@ -69,12 +76,43 @@ export function ScoreDataTable<TData, TValue>({
     getFacetedUniqueValues: getFacetedUniqueValues(),
   });
 
+  const handleUploadScore = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!assignmentName) return;
+
+    const file = e.target.files?.[0];
+    if (!file) {
+      return toast.error("Can not read file");
+    }
+
+    const buffer = await file.arrayBuffer();
+    const workBook = XLSX.read(buffer, { type: "buffer" });
+
+    const sheet = workBook.Sheets[workBook.SheetNames[3]];
+
+    const [scoreTable] = await worksheetToTables(sheet);
+
+    const scores = tableToObject(scoreTable[0], scoreTable.slice(1));
+
+    const payload = scores.map((e) => {
+      return {
+        id: e._ID,
+        name: e.Name,
+        score: e[assignmentName],
+      };
+    });
+
+    console.log(payload);
+
+    e.target.value = "";
+  };
+
   return (
     <div className="space-y-4">
       <DataTableToolbar
         table={table}
         selectorOptions={[]}
         isViewOptions={false}
+        handleImport={handleUploadScore}
       />
       <div className="rounded-md border">
         <Table>
