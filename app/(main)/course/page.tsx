6e6dfb4bdfdfd8671e2bd39f1d05f5485afc4c83
XiaoXuxxxx <73125291+XiaoXuxxxx@users.noreply.github.com>
2024-02-16 20:05:46 +0700

    feat: implement search box in courses page

--- app/(main)/course/page.tsx
+++ app/(main)/course/page.tsx
@@ -1,10 +1,34 @@
 "use client";
-import CourseCard from "@/components/features/course/course-card";
+import CourseCard, { CourseCardProps } from "@/components/features/course/course-card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import { useState } from 'react';
+
+const courseCardProps: CourseCardProps[] = [
+  {
+    courseId: 'cpe100',
+    courseName: 'Fundamental of Programming in C',
+    href: '/course/1',
+    studentAmount: 49,
+    teacherAmount: 2,
+    finishedTask: 3,
+    totalTask: 55,
+  },
+  {
+    courseId: 'cpe101',
+    courseName: 'Intro into com eng',
+    href: '/course/2',
+    studentAmount: 111,
+    teacherAmount: 4,
+    finishedTask: 12,
+    totalTask: 33,
+  },
+]
 
 const CoursePage = () => {
+  const [searchValue, setSearchValue] = useState('')
+
   return (
     <div className="container py-8">
       <div>
@@ -15,6 +39,7 @@ const CoursePage = () => {
           type="search"
           placeholder="Search..."
           className="dark:bg-input w-10/12"
+          onChange={(e) => setSearchValue(e.target.value)}
         />
         <Link href="/course/create" className="w-2/12 flex justify-end">
           <Button
@@ -27,10 +52,27 @@ const CoursePage = () => {
         </Link>
       </div>
       <div className="grid grid-cols-3 gap-5">
-        <CourseCard />
-        <CourseCard />
-        <CourseCard />
-        <CourseCard />
+        {courseCardProps
+          .filter(e => {
+            const lowerCaseSearchValue = searchValue.toLocaleLowerCase()
+
+            const lowerCaseCourseName = e.courseName.toLowerCase()
+            const lowerCaseCourseId = e.courseId.toLowerCase()
+
+            return lowerCaseCourseName.includes(lowerCaseSearchValue) || lowerCaseCourseId.includes(lowerCaseSearchValue)
+          })
+          .map((e, i) => {
+            return <CourseCard
+              key={i}
+              courseId={e.courseId}
+              courseName={e.courseName}
+              href={e.href}
+              studentAmount={e.studentAmount}
+              finishedTask={e.finishedTask}
+              totalTask={e.totalTask}
+              teacherAmount={e.teacherAmount}
+            />
+          })}
       </div>
     </div>
   );
