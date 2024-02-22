5757d37b855e48c555206ab9b850ad00eefd8267
Porping <porsteam@hotmail.com>
2024-02-22 14:09:24 +0700

    feat: implement lecturer edit dialog

---
+++ components/features/lecturer/lecturer-edit-dialog.tsx
@@ -0,0 +1,109 @@
+import { DialogClose } from '@radix-ui/react-dialog';
+
+import { Button } from '@/components/ui/button';
+import {
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useStrictForm } from '@/hooks/form-hook';
+import {
+  EditLecturerDefaultValues,
+  EditLecturerSchema,
+  EditLecturerType,
+} from '@/types/schema/lecturer-schema';
+
+type LecturerDialogProps = {
+  onSubmit: (values: EditLecturerType) => void;
+  defaultValues?: EditLecturerType;
+};
+
+const LecturerEditDialog: React.FC<LecturerDialogProps> = ({
+  onSubmit,
+  defaultValues,
+}) => {
+  const form = useStrictForm(
+    EditLecturerSchema,
+    defaultValues ?? EditLecturerDefaultValues,
+  );
+  return (
+    <div>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edit Lecturer</DialogTitle>
+          <DialogDescription>Edit the lecturer information</DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="firstName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Firstname</FormLabel>
+                  <FormControl>
+                    <div className="flex flex-col space-y-3">
+                      <Input {...field} />
+                      <FormMessage />
+                    </div>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="lastName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Lastname</FormLabel>
+                  <FormControl>
+                    <div className="flex flex-col space-y-3">
+                      <Input {...field} />
+                      <FormMessage />
+                    </div>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <div className="flex flex-col space-y-3">
+                      <Input {...field} />
+                      <FormMessage />
+                    </div>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant="outline">Cancel</Button>
+          </DialogClose>
+          <Button type="submit" onClick={form.handleSubmit(onSubmit)}>
+            Save
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </div>
+  );
+};
+
+export default LecturerEditDialog;
--- types/schema/lecturer-schema.ts
+++ types/schema/lecturer-schema.ts
@@ -32,3 +32,48 @@ export const CreateLecturerDefaultValues: CreateLecturerType = {
   password: '',
   confirmPassword: '',
 };
+
+export const EditLecturerSchema = z.object({
+  firstName: z
+    .string({ required_error: 'required' })
+    .min(1, { message: 'required' }),
+  lastName: z
+    .string({ required_error: 'required' })
+    .min(1, { message: 'required' }),
+  email: z
+    .string({ required_error: 'required' })
+    .min(1, { message: 'required' }),
+});
+
+export type EditLecturerType = z.infer<typeof EditLecturerSchema>;
+
+export const EditLecturerDefaultValues: EditLecturerType = {
+  firstName: '',
+  lastName: '',
+  email: '',
+};
+
+export const ChangePasswordSchema = z
+  .object({
+    oldPassword: z
+      .string({ required_error: 'required' })
+      .min(1, { message: 'required' }),
+    newPassword: z
+      .string({ required_error: 'required' })
+      .min(1, { message: 'required' }),
+    confirmNewPassword: z
+      .string({ required_error: 'required' })
+      .min(1, { message: 'required' }),
+  })
+  .refine((data) => data.newPassword === data.confirmNewPassword, {
+    message: "Password doesn't match",
+    path: ['confirmNewPassword'],
+  });
+
+export type ChangePasswordType = z.infer<typeof ChangePasswordSchema>;
+
+export const ChangePasswordDefaultValues: ChangePasswordType = {
+  oldPassword: '',
+  newPassword: '',
+  confirmNewPassword: '',
+};
