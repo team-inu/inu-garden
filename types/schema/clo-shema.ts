cf246bd619ed902ccec9c1a13fa899c8c7ece933
Porping <porsteam@hotmail.com>
2024-02-27 18:49:24 +0700

    feat: update CloSchema and CreateCloSchema

--- types/schema/clo-shema.ts
+++ types/schema/clo-shema.ts
@@ -2,6 +2,24 @@ import * as z from 'zod';
 
 import { GetSubProgramLearningOutcomeList } from '@/types/schema/sub-plo-schema';
 
+export const CloSchema = z.object({
+  id: z.string(),
+  code: z.string(),
+  description: z.string(),
+  expectedPassingAssignmentPercentage: z.number(),
+  expectedScorePercentage: z.number(),
+  expectedPassingStudentPercentage: z.number(),
+  courseId: z.string(),
+  status: z.string(),
+  programOutcomeId: z.string(),
+});
+
+type Clo = z.infer<typeof CloSchema>;
+
+export type CloColumn = Clo;
+
+export type GetCloResponse = Clo;
+
 const optionSchema = z.object({
   label: z.string(),
   value: z.string(),
@@ -27,7 +45,9 @@ export type GetCourseLearningOutcomeWithSubPlo = GetCourseLearningOutcome & {
 };
 
 export const CreateCloSchema = z.object({
-  code: z.string(),
+  code: z
+    .string({ required_error: 'required' })
+    .min(1, { message: 'required' }),
   description: z
     .string({ required_error: 'required' })
     .min(1, { message: 'required' }),
@@ -41,6 +61,9 @@ export const CreateCloSchema = z.object({
     required_error: 'required',
   }),
   status: z.string({ required_error: 'required' }).min(1, {
+    message: 'required',
+  }),
+  programLearningOutcomeId: z.string({ required_error: 'required' }).min(1, {
     message: 'required',
   }),
   subProgramLearningOutcomeId: z
@@ -60,6 +83,7 @@ export const CreateCloDefaultValues: CreateCloType = {
   expectedScorePercentage: 0,
   expectedPassingStudentPercentage: 0,
   status: '',
+  programLearningOutcomeId: '',
   subProgramLearningOutcomeId: [],
   programOutcomeId: '',
 };
@@ -72,4 +96,42 @@ export type CreateManyCloType = z.infer<typeof CreateManyCloSchema>;
 
 export const CreateManyCloDefaultValues: CreateManyCloType = {
   clo: [],
+};
+
+export const EditCloSchema = z.object({
+  id: z.string({ required_error: 'required' }).min(1, { message: 'required' }),
+  code: z
+    .string({ required_error: 'required' })
+    .min(1, { message: 'required' }),
+  description: z
+    .string({ required_error: 'required' })
+    .min(1, { message: 'required' }),
+  expectedPassingAssignmentPercentage: z.coerce.number({
+    required_error: 'required',
+  }),
+  expectedScorePercentage: z.coerce.number({
+    required_error: 'required',
+  }),
+  expectedPassingStudentPercentage: z.coerce.number({
+    required_error: 'required',
+  }),
+  status: z.string({ required_error: 'required' }).min(1, {
+    message: 'required',
+  }),
+  programOutcomeId: z.string({ required_error: 'required' }).min(1, {
+    message: 'required',
+  }),
+});
+
+export type EditCloType = z.infer<typeof EditCloSchema>;
+
+export const EditCloDefaultValues: EditCloType = {
+  id: '',
+  code: '',
+  description: '',
+  expectedPassingAssignmentPercentage: 0,
+  expectedScorePercentage: 0,
+  expectedPassingStudentPercentage: 0,
+  status: '',
+  programOutcomeId: '',
 };
