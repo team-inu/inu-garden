6173aa7437c0608e86ac74e9aa3462f2de95ea3c
XiaoXuxxxx <73125291+XiaoXuxxxx@users.noreply.github.com>
2024-02-27 21:18:16 +0700

    fix: status not show in enrollment table

--- components/features/course/enrollment/enrollment-column.tsx
+++ components/features/course/enrollment/enrollment-column.tsx
@@ -4,34 +4,9 @@ import { ColumnDef } from '@tanstack/react-table';
 
 import { EnrollmentRowActions } from '@/components/features/course/enrollment/enrollment-row-action';
 import { DataTableColumnHeader } from '@/components/ui/data-table-column-header';
-import { labels } from '@/data/data';
 import { EnrollmentColumn } from '@/types/schema/enrollment-schema';
 
 export const columns: ColumnDef<EnrollmentColumn>[] = [
-  // {
-  //   id: 'select',
-  //   header: ({ table }) => (
-  //     <Checkbox
-  //       checked={
-  //         table.getIsAllPageRowsSelected() ||
-  //         (table.getIsSomePageRowsSelected() && 'indeterminate')
-  //       }
-  //       onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-  //       aria-label="Select all"
-  //       className="translate-y-[2px]"
-  //     />
-  //   ),
-  //   cell: ({ row }) => (
-  //     <Checkbox
-  //       checked={row.getIsSelected()}
-  //       onCheckedChange={(value) => row.toggleSelected(!!value)}
-  //       aria-label="Select row"
-  //       className="translate-y-[2px]"
-  //     />
-  //   ),
-  //   enableSorting: false,
-  //   enableHiding: false,
-  // },
   {
     accessorKey: 'id',
     header: ({ column }) => (
@@ -88,22 +63,11 @@ export const columns: ColumnDef<EnrollmentColumn>[] = [
       <DataTableColumnHeader column={column} title="status" />
     ),
     cell: ({ row }) => {
-      const label = labels.find(
-        (label) => label.value === row.getValue('status'),
-      );
-
-      if (!label) {
-        return null;
-      }
-
       return (
-        <div className="flex w-[100px] items-center">
-          <span>{label.value}</span>
+        <div className="flex space-x-2">
+          <span className="truncate font-medium">{row.getValue('status')}</span>
         </div>
       );
-    },
-    filterFn: (row, id, value) => {
-      return value.includes(row.getValue(id));
     },
   },
   {
