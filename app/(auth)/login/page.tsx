444830e0e817d5ff930cad8a08b5eea54019c7db
Porping <porsteam@hotmail.com>
2024-03-02 21:52:57 +0700

    feat: remove unnecessary text in login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -60,7 +60,6 @@ const LoginPage = () => {
               <UserAuthForm />
             </div>
             <p className="px-8 text-center text-sm text-muted-foreground">
-              By clicking continue, you agree to our{' '}
               <Link
                 href="/terms"
                 className="underline underline-offset-4 hover:text-primary"
