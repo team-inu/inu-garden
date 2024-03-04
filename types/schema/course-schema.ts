aba4ab6fd9b8d7353a134ec0694142a95045853b
Porping <porsteam@hotmail.com>
2024-03-04 14:44:01 +0700

    refactor: course schema and form validation

--- types/schema/course-schema.ts
+++ types/schema/course-schema.ts
@@ -8,16 +8,15 @@ const CourseSchema = z.object({
   description: z.string(),
   semesterId: z.string(),
   userId: z.string(),
-  criteriaGrade: z.object({
-    criteriaGradeA: z.number(),
-    criteriaGradeBP: z.number(),
-    criteriaGradeB: z.number(),
-    criteriaGradeCP: z.number(),
-    criteriaGradeC: z.number(),
-    criteriaGradeDP: z.number(),
-    criteriaGradeD: z.number(),
-    criteriaGradeF: z.number(),
-  }),
+
+  criteriaGradeA: z.number(),
+  criteriaGradeBP: z.number(),
+  criteriaGradeB: z.number(),
+  criteriaGradeCP: z.number(),
+  criteriaGradeC: z.number(),
+  criteriaGradeDP: z.number(),
+  criteriaGradeD: z.number(),
+  criteriaGradeF: z.number(),
 });
 
 type Course = z.infer<typeof CourseSchema>;
@@ -49,59 +48,64 @@ export const CreateCourseSchema = z.object({
   graduateYear: z
     .string({ required_error: 'required' })
     .min(1, { message: 'required' }),
-
-  criteriaGrade: z.object({
-    criteriaGradeA: z.coerce.number({ required_error: 'Please enter A grade' }),
-    criteriaGradeBP: z.coerce.number({
-      required_error: 'Please enter BP grade',
-    }),
-    criteriaGradeB: z.coerce.number({ required_error: 'Please enter B grade' }),
-    criteriaGradeCP: z.coerce.number({
-      required_error: 'Please enter CP grade',
-    }),
-    criteriaGradeC: z.coerce.number({ required_error: 'Please enter C grade' }),
-    criteriaGradeDP: z.coerce.number({
-      required_error: 'Please enter DP grade',
-    }),
-    criteriaGradeD: z.coerce.number({ required_error: 'Please enter D grade' }),
-    criteriaGradeF: z.coerce.number({ required_error: 'Please enter F grade' }),
+  criteriaGradeA: z.coerce.number({ required_error: 'Please enter A grade' }),
+  criteriaGradeBP: z.coerce.number({
+    required_error: 'Please enter BP grade',
   }),
+  criteriaGradeB: z.coerce.number({ required_error: 'Please enter B grade' }),
+  criteriaGradeCP: z.coerce.number({
+    required_error: 'Please enter CP grade',
+  }),
+  criteriaGradeC: z.coerce.number({ required_error: 'Please enter C grade' }),
+  criteriaGradeDP: z.coerce.number({
+    required_error: 'Please enter DP grade',
+  }),
+  criteriaGradeD: z.coerce.number({ required_error: 'Please enter D grade' }),
+  criteriaGradeF: z.coerce.number({ required_error: 'Please enter F grade' }),
 });
 
 export type CreateCourseSchemaValues = z.infer<typeof CreateCourseSchema>;
 
 export const CreateCourseSchemaDefaultValues: Partial<CreateCourseSchemaValues> =
   {
-    criteriaGrade: {
-      criteriaGradeA: 80,
-      criteriaGradeBP: 75,
-      criteriaGradeB: 70,
-      criteriaGradeCP: 65,
-      criteriaGradeC: 60,
-      criteriaGradeDP: 55,
-      criteriaGradeD: 50,
-      criteriaGradeF: 45,
-    },
+    criteriaGradeA: 80,
+    criteriaGradeBP: 75,
+    criteriaGradeB: 70,
+    criteriaGradeCP: 65,
+    criteriaGradeC: 60,
+    criteriaGradeDP: 55,
+    criteriaGradeD: 50,
+    criteriaGradeF: 45,
   };
 
-export const UpdateCourseSchema = z.object({
-  department: z.string(),
-  programme: z.string(),
-  code: z.string(),
-  name: z.string(),
-  credit: z.string(),
-  education: z.string(),
-  user: z.string(),
+export const UpdateCourseSchema = CreateCourseSchema.pick({
+  name: true,
+  description: true,
+  code: true,
+  curriculum: true,
+  criteriaGradeA: true,
+  criteriaGradeBP: true,
+  criteriaGradeB: true,
+  criteriaGradeCP: true,
+  criteriaGradeC: true,
+  criteriaGradeDP: true,
+  criteriaGradeD: true,
+  criteriaGradeF: true,
 });
 
 export type UpdateCourseFormValues = z.infer<typeof UpdateCourseSchema>;
 
 export const UpdateCourseDefaultValues: UpdateCourseFormValues = {
-  department: '',
-  programme: '',
-  code: '',
   name: '',
-  credit: '',
-  education: '',
-  user: '',
+  description: '',
+  code: '',
+  curriculum: '',
+  criteriaGradeA: 80,
+  criteriaGradeBP: 75,
+  criteriaGradeB: 70,
+  criteriaGradeCP: 65,
+  criteriaGradeC: 60,
+  criteriaGradeDP: 55,
+  criteriaGradeD: 50,
+  criteriaGradeF: 45,
 };
