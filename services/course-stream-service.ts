0df86b6650a53157e49860ec1399f1c2f23b49a2
Porping <porsteam@hotmail.com>
2024-04-26 14:05:31 +0700

    feat: implement CourseStreamService class to handle course stream related API calls

---
+++ services/course-stream-service.ts
@@ -0,0 +1,30 @@
+import { ApiService } from '@/services/api-service';
+import { CreateCourseStreamPayload } from '@/types/schema/course-stream';
+
+class CourseStreamService extends ApiService {
+  public async getCourseStreamList() {
+    const url = '/course-streams';
+    return this.get(url)
+      .then((response) => {
+        return response.data.data;
+      })
+      .catch(this.throwError);
+  }
+
+  public async getCommentByCourseId(courseId: string) {
+    const url = `/courses/${courseId}/comments`;
+    return this.get(url)
+      .then((response) => {
+        return response.data.data;
+      })
+      .catch(this.throwError);
+  }
+
+  public async createCourseStream(courseStream: CreateCourseStreamPayload) {
+    const url = `/course-streams`;
+
+    return this.post(url, courseStream)
+      .then(() => courseStream)
+      .catch(this.throwError);
+  }
+}
