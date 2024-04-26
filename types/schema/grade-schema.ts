76e587e57a00339c3956b724e4f49ab59b3d0775
XiaoXuxxxx <aonrok555@gmail.com>
2024-04-26 21:05:59 +0700

    feat: edit year data type

--- types/schema/grade-schema.ts
+++ types/schema/grade-schema.ts
@@ -3,25 +3,27 @@ import * as z from 'zod';
 const GradeSchema = z.object({
   id: z.string(),
   semesterSequence: z.string(),
-  year: z.string(),
+  year: z.number(),
   studentId: z.string(),
   grade: z.string(),
 });
 
 export const CreateGradeSchema = z.object({
   semesterSequence: z.string(),
-  year: z.string(),
+  year: z.number(),
   studentId: z.string(),
   grade: z.string(),
 });
 
-const PayloadCreateGradeSchema = z.object({
+export const PayloadCreateGradeSchema = z.object({
   semesterSequence: z.string(),
-  year: z.string(),
-  studentGrade: z.object({
-    studentId: z.string(),
-    grade: z.string(),
-  }),
+  year: z.number(),
+  studentGrade: z
+    .object({
+      studentId: z.string(),
+      grade: z.string(),
+    })
+    .array(),
 });
 
 export type GradeType = z.infer<typeof GradeSchema>;
@@ -32,7 +34,13 @@ export type PayloadCreateGradeType = z.infer<typeof PayloadCreateGradeSchema>;
 
 export const CreateGradeTypeDefaultValues: CreateGradeType = {
   semesterSequence: '',
-  year: '',
+  year: 0,
   studentId: '',
   grade: '',
+};
+
+export const PayloadCreateGradeTypeDefaultValues: PayloadCreateGradeType = {
+  semesterSequence: '',
+  year: 0,
+  studentGrade: [],
 };
