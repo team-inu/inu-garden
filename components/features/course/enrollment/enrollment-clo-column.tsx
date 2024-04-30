2fe13d5ae3d895815669b1ddfe1e94c88eda9199
Porping <porsteam@hotmail.com>
2024-05-01 00:05:59 +0700

    feat: add enrollment CLO column to course enrollment feature

---
+++ components/features/course/enrollment/enrollment-clo-column.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import { ColumnDef } from '@tanstack/react-table';
+
+import { DataTableColumnHeader } from '@/components/ui/data-table-column-header';
+import { EnrollmentCloColumn } from '@/types/schema/enrollment-schema';
+
+export const columns: ColumnDef<EnrollmentCloColumn>[] = [
+  // {
+  //   accessorKey: 'id',
+  //   header: ({ column }) => (
+  //     <DataTableColumnHeader column={column} title="EnrollmentId" />
+  //   ),
+  //   cell: ({ row }) => <div className="w-[80px]">{row.getValue('id')}</div>,
+  //   enableSorting: false,
+  //   enableHiding: false,
+  // },
+  {
+    accessorKey: 'studentId',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="kmutt id" />
+    ),
+    cell: ({ row }) => (
+      <div className="w-[80px]">{row.getValue('studentId')}</div>
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: 'firstName',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="First Name" />
+    ),
+    cell: ({ row }) => {
+      return (
+        <div className="flex space-x-2">
+          <span className="truncate font-medium">
+            {row.getValue('firstName')}
+          </span>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: 'lastName',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Last Name" />
+    ),
+    cell: ({ row }) => {
+      return (
+        <div className="flex space-x-2">
+          <span className="truncate font-medium">
+            {row.getValue('lastName')}
+          </span>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: 'pass',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Status" />
+    ),
+    cell: ({ row }) => {
+      return (
+        <div className="flex space-x-2">
+          <span className="truncate font-medium">
+            {row.getValue('pass') ? 'pass' : 'failed'}
+          </span>
+        </div>
+      );
+    },
+  },
+];
