11d875f633044f25a9c4317ce4de48e038c8288d
Porping <porsteam@hotmail.com>
2024-05-04 02:10:34 +0700

    feat: Update PredictionPage with "Coming Soon...." message

--- app/(main)/prediction/page.tsx
+++ app/(main)/prediction/page.tsx
@@ -1,5 +1,9 @@
 const PredictionPage = () => {
-  return <div>this is prediction page</div>;
+  return (
+    <div className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden">
+      <div className="animate-pulse text-3xl font-bold">Coming Soon....</div>
+    </div>
+  );
 };
 
 export default PredictionPage;
